a8ff4959d3acebc2de5bfed53d76bfe4981a5a16
Lucas Jackson <lucasjackson5815@gmail.com>
2024-02-14 15:04:26 -0800

    add retriever to indexify_langchain

--- langchain/indexify_langchain/__init__.py
+++ langchain/indexify_langchain/__init__.py
@@ -1,4 +1,6 @@
 from indexify_langchain.memory.indexify import IndexifyMemory
+from indexify_langchain.retriever import IndexifyRetriever
+
 from indexify import DEFAULT_SERVICE_URL
 
-__all__ = ["IndexifyMemory", "DEFAULT_SERVICE_URL"]
+__all__ = ["IndexifyMemory","IndexifyRetriever", "DEFAULT_SERVICE_URL"]
