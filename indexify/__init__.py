7bafc70871cb997bc335585f2b09a9689b580fc1
Rishiraj Acharya <44090649+rishiraj@users.noreply.github.com>
2024-08-06 20:34:49 +0530

    Update __init__.py

--- indexify/__init__.py
+++ indexify/__init__.py
@@ -4,6 +4,7 @@ from .extraction_policy import ExtractionGraph
 from .client import IndexifyClient, Document, generate_hash_from_string, generate_unique_hex_id
 from .data_containers import Content
 from .settings import DEFAULT_SERVICE_URL
+from .document_loader import simple_directory_loader
 
 __all__ = [
     "Index",
@@ -15,4 +16,5 @@ __all__ = [
     "DEFAULT_SERVICE_URL",
     "generate_hash_from_string",
     "generate_unique_hex_id",
+    "simple_directory_loader",
 ]
